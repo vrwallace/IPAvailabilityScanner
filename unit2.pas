4c602ea58d882a4578c8931a0c51f79397abb7fa
Von Wallace <vonwallace@yahoo.com>
2023-11-25 10:31:57 -0600

    Open in default application for Port scan ports
    Right click on Port 80 result and open browser to IP etc

--- unit2.pas
+++ unit2.pas
@@ -5,7 +5,7 @@ unit Unit2;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Grids, StdCtrls,menus,clipbrd;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Grids, StdCtrls,menus,clipbrd,Windows, ShellAPI;
 
 type
 
@@ -16,6 +16,8 @@ type
     StringGridResults: TStringGrid;
     procedure FormCreate(Sender: TObject);
      procedure CopySelectedCells(Sender: TObject);
+     procedure OpenInDefaultApp(Sender: TObject);
+     procedure OpenURL(URL: string);
   private
 
   public
@@ -31,10 +33,17 @@ implementation
 
 { TFormScanResults }
 
+
+procedure TFormScanResults.OpenURL(URL: string);
+begin
+  ShellExecute(0, 'open', PChar(URL), nil, nil, SW_SHOWNORMAL);
+end;
+
+
 procedure TFormScanResults.FormCreate(Sender: TObject);
 
   var
-  MenuItemcopy: TMenuItem;
+  MenuItemcopy,MenuItemOpen: TMenuItem;
   begin
  // inherited Create(AOwner);
 
@@ -42,13 +51,60 @@ procedure TFormScanResults.FormCreate(Sender: TObject);
 
   // Create popup menu
  stringgridresults.PopupMenu := TPopupMenu.Create(stringgridresults);
+
+   // Open MenuItem
+  MenuItemOpen := TMenuItem.Create(PopupMenu);
+  MenuItemOpen.Caption := 'Open in Default Application';
+  MenuItemOpen.OnClick := @OpenInDefaultApp;
+  stringgridresults.PopupMenu.Items.Add(MenuItemOpen);
+
+
   MenuItemCopy := TMenuItem.Create(stringgridresults.PopupMenu);
   MenuItemCopy.Caption := 'Copy (Multiple)';
   MenuItemCopy.OnClick := @CopySelectedCells;
   stringgridresults.PopupMenu.Items.Add(MenuItemCopy);
 
 
+
+
+
 end;
+ procedure TFormScanResults.OpenInDefaultApp(Sender: TObject);
+var
+  SelectedRow: Integer;
+  IPAddress, PortStr: string;
+  Port: Integer;
+  URL: string;
+begin
+  SelectedRow := StringGridResults.Row;
+  if (SelectedRow > 0) and (SelectedRow < StringGridResults.RowCount) then
+  begin
+    IPAddress := StringGridResults.Cells[0, SelectedRow];
+    PortStr := StringGridResults.Cells[1, SelectedRow];
+
+    if TryStrToInt(PortStr, Port) then
+    begin
+      case Port of
+        80: URL := 'http://' + IPAddress+':'+inttostr(port);
+        443: URL := 'https://' + IPAddress+':'+inttostr(port);
+        22: URL := 'ssh://' + IPAddress+':'+inttostr(port);
+        21: URL := 'ftp://' + IPAddress+':'+inttostr(port);
+        23: URL := 'telnet://' + IPAddress+':'+inttostr(port); // Assuming the system has a telnet handler
+        3389: URL := 'rdp://' + IPAddress+':'+inttostr(port); // RDP - Specific to applications that can handle RDP URLs
+        // Add additional ports and protocols as needed
+      else
+        Exit; // Do nothing for unknown ports
+      end;
+
+      // Open the URL with the default application
+      if URL <> '' then
+        OpenURL(URL);
+    end;
+  end;
+end;
+
+
+
      procedure TFormScanResults.CopySelectedCells(Sender: TObject);
 var
   ClipBoardText: string;
